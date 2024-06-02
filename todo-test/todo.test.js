d921a3809dd43347fbfea28f73de130f9b9aa3a0
Yordan Valkov <yordan.valkov@gmail.com>
2024-06-02 10:54:00 +0300

    Add test for delete specific element

--- todo-test/todo.test.js
+++ todo-test/todo.test.js
@@ -8,8 +8,8 @@ await page.goto(pageUrl);
 await expect(page).toHaveURL(pageUrl);
 });
 
-// Test 2: Test If a User Can Add a Task
 
+// Test 2: Test If a User Can Add a Task
 test('User can add a task', async ({page})=> {
     await page.goto(pageUrl);
     await page.fill('#task-input', 'New Task');
@@ -18,3 +18,49 @@ test('User can add a task', async ({page})=> {
     
     await expect(taskText).toContain('New Task');
     });
+
+
+//Test 3: Test If a User Can Delete a Task
+test('User can delete a task', async({page})=>
+        {
+        await page.goto(pageUrl);
+        
+        await page.fill('#task-input', 'New Task 1');
+        await page.click('#add-task');
+        
+        await page.fill('#task-input', 'New Task 2');
+        await page.click('#add-task');
+        
+        await page.fill('#task-input', 'New Task 3');
+        await page.click('#add-task');
+        
+        const firstTaskText = await page.textContent('#task-list');
+        await expect(firstTaskText).toContain('New Task 1');
+        
+        const secondTaskText = await page.textContent('#task-list');
+        await expect(secondTaskText).toContain('New Task 2');
+        
+        const thirdTaskText = await page.textContent('#task-list');
+        await expect(thirdTaskText).toContain('New Task 3');
+        
+        const taskToDelete = "New Task 2";
+        
+        const taskLocator = page.locator(`#task-list .task:has-text("${taskToDelete}")`);
+        
+          // Ensure the specific task is present
+        await expect(taskLocator).toContainText(taskToDelete);
+         // Click the delete button for the specific task
+        await taskLocator.locator('.delete-task').click();
+        
+          // Ensure the task is deleted
+        await expect(page.locator('#task-list')).not.toContainText(taskToDelete);
+});
+        
+        
+        
+        
+//Test 4: Test If a User Can Mark a Task as Complete
+        
+        
+        
+//Test 5: Test If a User Can Filter Tasks
