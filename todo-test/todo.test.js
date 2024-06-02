fce5c7088dcf2055c5b2a48d94e2be4e7df4407d
Yordan Valkov <yordan.valkov@gmail.com>
2024-06-02 10:52:10 +0300

    Add test for add a task

--- todo-test/todo.test.js
+++ todo-test/todo.test.js
@@ -8,3 +8,13 @@ await page.goto(pageUrl);
 await expect(page).toHaveURL(pageUrl);
 });
 
+// Test 2: Test If a User Can Add a Task
+
+test('User can add a task', async ({page})=> {
+    await page.goto(pageUrl);
+    await page.fill('#task-input', 'New Task');
+    await page.click('#add-task');
+    const taskText = await page.textContent('.task');
+    
+    await expect(taskText).toContain('New Task');
+    });
